efdf98634be5bd26b58bf277cf6864fc8e0a4215
Nuradiyana <me@nooradiana.com>
2024-06-01 19:41:05 +0700

    Handle register

--- resources/ts/pages/auth/register.tsx
+++ resources/ts/pages/auth/register.tsx
@@ -1,19 +1,10 @@
 import { Fragment, ReactNode } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from "~/components/ui/card";
-import { LoginForm } from "./components/login-form";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 import { useTheme } from "~/components/theme-provider";
 import { Link } from "@inertiajs/react";
-import { useFortifyFeature } from "~/hooks/use-fortify";
+import { RegisterForm } from "~/pages/auth/components/register-form";
 
 function Page() {
-  const feature = useFortifyFeature();
   const theme = useTheme();
 
   return (
@@ -21,21 +12,16 @@ function Page() {
       <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8">
         <Card className="mx-auto lg:w-full max-w-md">
           <CardHeader>
-            <CardTitle className="text-2xl">Masuk akunmu</CardTitle>
+            <CardTitle className="text-2xl">Buat akun barumu</CardTitle>
             <CardDescription>
-              Masukkan email Anda di bawah ini untuk login ke akun Anda.
+              Masukan email dan password untuk membuat akun baru kamu, atau sudah punya akun?{" "}
+              <Link href="/login" className="underline underline-offset-4 hover:text-primary">
+                masuk disini
+              </Link>
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <LoginForm />
-            {feature.registration && (
-              <CardDescription className="pt-4">
-                Belum punya akun?{" "}
-                <Link href="/register" className="underline underline-offset-4 hover:text-primary">
-                  Kamu bisa buat satu disini
-                </Link>
-              </CardDescription>
-            )}
+            <RegisterForm />
           </CardContent>
         </Card>
         <div className="pt-2 flex items-center justify-center">
